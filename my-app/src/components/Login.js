1c5dc848ef78c18573ca6577e3f2b9a9528ca314
Virginia Davenport <virginialdport@gmail.com>
2020-05-26 11:24:00 -0600

    added validation to login

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory, Link } from 'react-router-dom'
+import * as yup from 'yup'
 
 const initialState= {
     username: '',
@@ -11,14 +12,46 @@ const initialFormErrors={
     password: 'Password is required',
 }
 
+const loginSchema = yup.object().shape({
+    username: yup.string()
+        .min(3, 'Username must be at least 3 characters long')
+        .required('Username is a required field'),
+    password: yup.string()
+        .min(8, 'Password must be at least 8 characters long')
+        .required('Password is a required field')
+    
+})
+
 const Login = () => {
     
     let history = useHistory()
 
-    const [loginData, setLoginData] = useState({username: '', password: ''})
+    const [loginData, setLoginData] = useState(initialState)
+    const [loginFormErrors, setLoginFormErrors] = useState(initialFormErrors)
+    const [buttonEnabled, setButtonEnabled] = useState(false)
+
+
+    useEffect(() => {
+        loginSchema.isValid(loginData).then((valid) => {
+            setButtonEnabled(valid)
+        })
+    }, [loginData])
 
     const loginChangeHandler = (evt) => {
         setLoginData({...loginData, [evt.target.name]: evt.target.value})
+
+        yup
+            .reach(loginSchema, evt.target.name)
+            .validate(evt.target.value)
+            .then((valid) => {
+                setLoginFormErrors({...loginFormErrors, [evt.target.name]: ''})
+            })
+            .catch((err) => {
+                setLoginFormErrors({
+                    ...loginFormErrors,
+                    //[evt.target.name]: err.errors[0]
+                })
+            })
     }
 
     const loginHandler = (evt) => {
@@ -36,6 +69,7 @@ const Login = () => {
                 <input 
                     type='text'
                     name='username'
+                    placeholder='Username'
                     value={loginData.username}
                     onChange={(evt)=> loginChangeHandler(evt)}
                 />
@@ -44,11 +78,12 @@ const Login = () => {
                 <input 
                     type='password'
                     name='password'
+                    placeholder='Password'
                     value={loginData.password}
                     onChange={(evt)=> loginChangeHandler(evt)}
                 />
 
-                <button type='submit'>Login</button>
+                <button disabled={!buttonEnabled} type='submit'>Login</button>
             </form>
 
         </div>
