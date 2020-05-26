5979bdf56591ad2db8cfbc5dcf5ba318c1f809c8
Brian Russell <bruss@brians-mbp.lan>
2020-05-26 16:19:08 -0400

    Created redux store, added utilities, and added more components

---
+++ my-app/src/store/reducers/recipeReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  recipies: [],
+  isFetching: false,
+  isEditing: false,
+}
+
+export const recipeReducer = (state = initialState, action) => {
+  switch(action.type){
+    default: return state
+  }
+}
