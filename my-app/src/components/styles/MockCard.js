787da2caf00fe114b3bbe3303d30da1d821cefc2
Brian Russell <bruss@brians-mbp.lan>
2020-05-28 13:35:55 -0400

    Added some styling

--- my-app/src/components/styles/MockCard.js
+++ my-app/src/components/styles/MockCard.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const MockCard = styled.div`
     display:flex;
     flex-direction:column;
-    //align-items:flex-end;
+    /* align-items:flex-end; */
     background-color: gray;
     width: 45%;
     margin: 10px;
