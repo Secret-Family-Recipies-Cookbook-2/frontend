7caa312b2a8594138108e247dc442d5ca731e793
Virginia Davenport <virginialdport@gmail.com>
2020-05-28 15:25:42 -0600

    styles

--- my-app/src/components/MockRecipe.js
+++ my-app/src/components/MockRecipe.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MockCard from './styles/MockCard'
 import styled from 'styled-components'
 
 const MockRecipe = ({details}) => {
+   const [showDetails, setShowDetails] = useState(false)
+
+   const toggleDetails = evt => {
+       evt.preventDefault()
+       setShowDetails(!showDetails)
+   }
+   
     if(!details){
         return <h3>Working on fetching details</h3>
     }
@@ -13,8 +20,21 @@ const MockRecipe = ({details}) => {
             <h2>{details.title}</h2>
             <h4>Category: {details.category}</h4>
             <h4>Source: {details.source}</h4>
-            <p>Ingredients:{details.ingredients}</p>
-            <p>Instructions:{details.instructions}</p>
+
+            <button onClick={evt => {toggleDetails(evt)}}>
+                {
+                    showDetails ? 'Hide Details':'Show Details'
+                }
+            </button>
+            {
+                showDetails && (
+                    <>
+                        <p>Ingredients:{details.ingredients}</p>
+                        <p>Instructions:{details.instructions}</p>
+                    </>
+                )
+            }
+          
          </MockCard>
         
     )
--- my-app/src/components/styles/LoginCard.js
+++ my-app/src/components/styles/LoginCard.js
@@ -4,26 +4,34 @@ const LoginCard = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    align-items: center;
     width: 45%;
     background-color:#DCAAFF;
     margin: 10px;
     padding: 20px 10px;
     border-radius: 10px;
+    text-align: center;
 
     button{
         background-color: #0000CC;
         color:white;
         border-radius: 5px;
+        border: 3px solid #AAAAFF;
+        font-size: .75rem;
     }
 
     input{
         border-radius: 5px;
         border: none;
+        margin: 3px;
        
     }
-
+    label{
+        display: flex;
+        flex-direction: column;
+    }
     .no-account{
-        font-size: .5rem;
+        font-size: .75rem;
     }
 `;
 export default LoginCard
